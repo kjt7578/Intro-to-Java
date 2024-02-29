6194404daa6197a4e5a4a447615be137a0cb858f
JT <56941721+kjt7578@users.noreply.github.com>
2024-02-29 23:19:30 +0000

    Staircase Builder

--- ass4/StaircaseBuilder.java
+++ ass4/StaircaseBuilder.java
@@ -16,6 +16,37 @@ public class StaircaseBuilder {
     
     public static void main(String[] args) {
 
-        // WRITE YOUR CODE HERE
+      int d = Integer.parseInt(args[0]);
+      int num = Integer.parseInt(args[1]);
+
+      char block [][];                    // 3 6
+      block = new char[d][d];             // 2 2 (0~2) (0~2)
+      for(int i = 0; i < d; i++){         //Starting from row 0
+
+          for(int j = 0; j < d; j++){
+            block[j][i] = ' ';
+          }
+
+        
+        for(int j = d-1; j >= d-i-1; j--){      //Starting from end of col
+          if(num > 0){
+          block[j][i] = 'X';
+          num--;
+          }
+         }
+      }
+
+
+      
+      for(int i = 0; i < d; i++){        // Print
+        for(int j = 0; j < d; j++){
+          System.out.print(block[i][j]);
+        }
+        System.out.println("");
+      }
+
+
+      System.out.println("Bricks remaining: " + num);
     }
-}
+      
+  }
